d068a648bdce673bca763dd89360b730785045aa
mlogic1 <multilogic@outlook.com>
2021-11-27 19:37:06 +0100

    updated android system methods

--- include/android/SystemAndroid.h
+++ include/android/SystemAndroid.h
@@ -9,11 +9,11 @@
 #include <android/asset_manager_jni.h>
 #include <string>
 
-
 namespace System
 {
 	
 	extern AAssetManager* NDK_ASSET_MANAGER;
+	extern JNIEnv *JAVA_ENV;
 
 	class SystemAndroid : public SystemBase
 	{
@@ -23,23 +23,34 @@ namespace System
 			bool IsRunning() override;
 			virtual KeyState GetKeyState(const Key& key) override;
 			virtual Vector2i GetWindowDimensions() const override;
+			void SetWindowDimensions(Vector2i dimensions);
 			// TODO getsystemtype
 
 			bool LoadBinaryDataFromAssets(const std::string fileName, unsigned char*& data, off_t& length) const override;
 			bool LoadStringDataFromAssets(const std::string fileName, std::string& data) const override;
 			//bool SetSystemAssetManager(AAssetManager *assetManager);
+
+			void SetCursorPosition(int x, int y);
+			void OnCursorPressed(int x, int y);
+			void OnCursorReleased();
+
+		private:
+			double GetCurrentTime() const;
+
+		private:
+			Vector2i m_windowDimensions;
+			std::vector<Key> m_pressedKeys;
+
+			double prevTime = 0;
+			int frameCounter = 0;
+
+			int m_viewport[4];
 	};
 
+	void SetSystemEnvPointer(JNIEnv *env);
 	void SetSystemAssetManager(AAssetManager *assetManager);
     /*  System functions  */
-	//bool InitSystemAndroid();
-	
-	//bool IsRunning();
-	//void update();
-
-
-	//bool LoadBinaryDataFromAssets(const std::string fileName, unsigned char*& data, off_t& length);
-	//bool LoadStringDataFromAssets(const std::string fileName, std::string& data);
+	// bool InitSystemAndroid();
 }
 
 #endif // SYSTEM_ANDROID_H
--- src/android/SystemAndroid.cpp
+++ src/android/SystemAndroid.cpp
@@ -5,11 +5,12 @@
 #include <EGL/egl.h>
 #include <glad/glad.h>
 #include <SceneManager.h>
+#include <time.h>
 
 namespace System
 {
     AAssetManager* NDK_ASSET_MANAGER = nullptr;
-
+    JNIEnv* JAVA_ENV = nullptr;
 
 	bool SystemAndroid::InitSystem()
 	{
@@ -17,23 +18,20 @@ namespace System
 		
         if(!gladLoadGLES2Loader((GLADloadproc)eglGetProcAddress))
         {
-            Log::Write("Failed to initialize glad");
-            initSuccess = false;
-        }
+			Log::Write("Failed to initialize glad");
+			initSuccess = false;
+		}
 
+        m_windowDimensions.Set(1920, 1080);
 		return initSuccess;
 	}
 
-	/*bool SystemAndroid::SetSystemAssetManager(AAssetManager* assetManager)
-	{
-        NDK_ASSET_MANAGER = assetManager;
-		return  true;
-	}*/
-
 	void SystemAndroid::Update()
 	{
-        float dt = 0.01f;
-		m_sceneManager->Update(dt);
+		double currentTime = GetCurrentTime();
+		m_deltaTime = (currentTime - prevTime) / 1000.0f;
+		prevTime = currentTime;
+		m_sceneManager->Update(m_deltaTime);
 	}
 
 	bool SystemAndroid::IsRunning()
@@ -44,13 +42,26 @@ namespace System
 
 	KeyState SystemAndroid::GetKeyState(const Key& key)
 	{
+		if (std::count(m_pressedKeys.cbegin(), m_pressedKeys.cend(), key) > 0)
+		{
+			return KeyState::PRESSED;
+		}
 		return KeyState::RELEASED;	// TODO
 	}
 
 	Vector2i SystemAndroid::GetWindowDimensions() const
 	{
-		return Vector2i(1920, 1080);	// TODO
+		return m_windowDimensions;
 	}
+
+    void SystemAndroid::SetWindowDimensions(Vector2i dimensions)
+    {
+	    m_windowDimensions = dimensions;
+		m_viewport[0] = 0;
+		m_viewport[1] = 0;
+		m_viewport[2] = dimensions.x;
+		m_viewport[3] = dimensions.y;
+    }
 
 	bool SystemAndroid::LoadBinaryDataFromAssets(const std::string fileName, unsigned char*& data, off_t& length) const
 	{
@@ -94,6 +105,46 @@ namespace System
 		return true;
 	}
 
+	void SystemAndroid::SetCursorPosition(int x, int y)
+	{
+		// int viewport[4];
+		// glGetIntegerv(GL_VIEWPORT, viewport);	// returns random values for some reason? using a cached value instead
+		Vector2i windowDimensions = GetWindowDimensions();
+		const int windowWidth = windowDimensions.x;
+		const int windowHeight = windowDimensions.y;
+
+		// Log::Write("Viewport: " + std::to_string(m_viewport[0]) + "\t" + std::to_string(m_viewport[1]) + "\t" + std::to_string(m_viewport[2]) + "\t" + std::to_string(m_viewport[3]));
+
+		// convert to virtual resolution coordinates
+		float cursorPosX = ((float)x / windowWidth) * VIRTUAL_RESOLUTION_WIDTH;
+		float cursorPosY = ((float)y / windowHeight) * VIRTUAL_RESOLUTION_HEIGHT;
+		// float cursorPosX = ((x - m_viewport[0]) / windowWidth) / (static_cast<float>(m_viewport[2]) / windowWidth) * VIRTUAL_RESOLUTION_WIDTH;
+		// float cursorPosY = ((y - m_viewport[1]) / windowHeight) / (static_cast<float>(m_viewport[3]) / windowHeight) * VIRTUAL_RESOLUTION_HEIGHT;
+
+		Log::Write("Engine update cursor pos: " + std::to_string(cursorPosX) + ", " + std::to_string(cursorPosY));
+
+		m_cursorPosition.Set(cursorPosX, cursorPosY);
+	}
+
+	void SystemAndroid::OnCursorPressed(int x, int y)
+	{
+		m_pressedKeys.push_back(Key::CURSOR);
+		SetCursorPosition(x, y);
+	}
+
+	void SystemAndroid::OnCursorReleased()
+	{
+		m_pressedKeys.erase(std::remove(m_pressedKeys.begin(), m_pressedKeys.end(), Key::CURSOR), m_pressedKeys.end());
+		m_cursorPosition.Set(-1, -1);
+	}
+
+	double SystemAndroid::GetCurrentTime() const
+	{
+		struct timespec now;
+		clock_gettime(CLOCK_REALTIME, &now);
+		return 1000.0 * now.tv_sec + (double) now.tv_nsec / 1e6;
+	}
+
 	bool InitGameEngine()
 	{
 		SYSTEM_PTR = new SystemAndroid();
@@ -103,12 +154,16 @@ namespace System
 		SYSTEM_PTR->InitTextureManager();
 		SYSTEM_PTR->InitFontManager();
 		SYSTEM_PTR->SetupEvents();
-		
 		return true;
 	}
 
 	void SetSystemAssetManager(AAssetManager *assetManager)
 	{
 		NDK_ASSET_MANAGER = assetManager;
+	}
+
+	void SetSystemEnvPointer(JNIEnv *env)
+	{
+		JAVA_ENV = env;
 	}
 }
